09a692270816ae391aa4650641ee132d4f4e4ae9
Kyle Jensen <kjensen19@gmail.com>
2022-08-05 20:49:06 -0500

    cleaning function formatting up

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -7,45 +7,46 @@ console.log('***** Function Practice *****')
 // 1. Function to return 'Hello World!'
 function hello() {
   return 'Hello World!';
-}
+};
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
 
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
+function helloName(name) {
   return `Hello, ${name}!`;
-}
+};
 // Remember to call the function to test
 console.log('Test- should say "Hello, name provided!"', helloName('Kyle'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber, secondNumber ) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
   return firstNumber + secondNumber;
-}
+};
 
 console.log('Test- should return the sum of provided numbers:', addNumbers(7,8));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree(firstNum, secondNum, thirdNum){
+function multiplyThree(firstNum, secondNum, thirdNum) {
   return firstNum * secondNum * thirdNum;
-}
+};
 
 console.log('Test - should return the 3 numbers multiplied:', multiplyThree(3,4,5));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ){
+function isPositive(number) {
+  if (number > 0){
     return true;
   }
   else{
     return false;
   }
-}
+};
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
@@ -55,24 +56,24 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
-  if (array.length === 0){
+function getLast(array) {
+  if (array.length === 0) {
     return undefined;
   }
   else {
-    return array[array.length - 1]
+    return array[array.length - 1];
   }
 
-}
+};
 console.log('Test - should return 7', getLast([4, 3, 6, 7]))
 console.log('Test- should return undefined', getLast([]));
 console.log('Test - should return D', getLast(['A', 'B', 'C', 'D']));
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ){
-  for (let i=0; i<array.length; i++){
-    if (array[i] === value){
+function find( value, array ) {
+  for (let i=0; i<array.length; i++) {
+    if (array[i] === value) {
       return true;
     }
   }
@@ -88,8 +89,8 @@ console.log('Test - should return true', find(('B'), ['A', 'B', 'C', 'D']));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-  if(string[0] === letter){
-    return true
+  if (string[0] === letter){
+    return true;
   }
   return false;
 }
@@ -100,13 +101,13 @@ console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 // 9. Function to return the sum of all numbers in an array
 function sumAll(array) {
   let sum = 0
-  for(num of array){
+  for (let num of array) {
     sum += num;
   }
   
   // TODO: loop to add items
   return sum;
-}
+};
 console.log('Test- should return 6', sumAll([1, 2, 3]));
 console.log('Test- should return 4', sumAll([-1, 2, 3]));
 console.log('Test- should return 0', sumAll([]));
@@ -117,13 +118,13 @@ console.log('Test- should return 0', sumAll([]));
 
 function keepItPos(array){
   let newArr = [];
-  for(num of array){
+  for (let num of array) {
     if (num > 0){
       newArr.push(num);
     }
   }
   return newArr;
-}
+};
 let test = [1, 2, 3];
 console.log('Test - should return []', keepItPos([-1, 0, -5]));
 console.log('Test - should return [7, 2]', keepItPos([-1, 7, 2]));
